3d3200f77dc723fd1883cea6b79a03ea154458a6
szobek69420 <szobek.adam@gmail.com>
2025-06-03 02:30:17 +0200

    big chungus

---
+++ officedaytime/www.officedaytime.com/tips/simdfeedback/alog.js
@@ -0,0 +1,14 @@
+﻿
+function alog(page)
+{
+	var alogurl = "/ct/alog.php";
+	alogurl += "?page=" + page;
+	alogurl += "&url=" + encodeURIComponent(document.location);
+	alogurl += "&ref=" + encodeURIComponent(document.referrer);
+	
+	document.writeln('<script type="text/javascript" id="alogjsframe"></script>');
+	document.getElementById('alogjsframe').setAttribute('src', alogurl);
+	document.writeln('<a href="mailto:daytime' + String.fromCharCode(8 * 8) + 'officedaytime.com">Daytime &lt;daytime' + String.fromCharCode(8 * 8) + 'officedaytime.com&gt;</a>');	
+}
+
+
